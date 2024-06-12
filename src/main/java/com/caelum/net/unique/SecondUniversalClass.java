a92b7a8016c0eecbdc78c7e9e5732780a1661e4c
mavianDev <hnotes@bk.ru>
2024-06-12 12:33:26 -0500

    fixed

--- src/main/java/com/caelum/net/unique/SecondUniversalClass.java
+++ src/main/java/com/caelum/net/unique/SecondUniversalClass.java
@@ -55,26 +55,15 @@ public class SecondUniversalClass implements Runnable {
     }
 
     public static String whoLikesIt(String ...names) {
-        if(names.length == 0) {
-            return "no one likes this";
-        }
+        return switch (names.length) {
+            case 0 -> "no one likes this";
+            case 1 -> names[0] + " like this";
+            case 2 -> names[0] + " and " + names[1] + " like this";
+            case 3 -> names[0] + ", " + names[1] + " and " + names[2] + "like this";
+            case 4 -> names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
+            default -> "";
+        };
 
-        else if(names.length == 1) {
-            return names[0] + " likes this";
-        }
-
-        else if(names.length == 2) {
-            return names[0] + " and " + names[1] + " like this";
-        }
-
-        else if(names.length == 3) {
-            return names[0] + ", " + names[1] + " and " + names[2] + " like this";
-        }
-
-        else if (names.length > 3) {
-            return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
-        }
-        return "";
     }
 
     public static int findIt (int[] a) {
@@ -86,8 +75,4 @@ public class SecondUniversalClass implements Runnable {
 
         return xor;
     }
-
-//    public Exception CheckSite (Boolean SSLConnection) throws IOException {
-//
-//    }
 }
